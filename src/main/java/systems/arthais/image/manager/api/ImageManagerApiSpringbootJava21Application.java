e7c61c734847c85c3574110caa4c0e6d121de385
arthais-systems <espirito.santo@arthais.systems>
2023-11-20 18:59:36 -0300

    Carga inicial

---
+++ src/main/java/systems/arthais/image/manager/api/ImageManagerApiSpringbootJava21Application.java
@@ -0,0 +1,13 @@
+package systems.arthais.image.manager.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImageManagerApiSpringbootJava21Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImageManagerApiSpringbootJava21Application.class, args);
+	}
+
+}
