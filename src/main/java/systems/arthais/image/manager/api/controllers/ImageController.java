62ed844caae52ffa84b2ce4e6041cc7cec5c0f1e
Raimundo do Espírito Santo <espirito.santo.arthais@outlook.com>
2023-11-21 07:12:08 -0300

    Validação dos parâmetros dos end-points com anotações e testes (if/else)

--- src/main/java/systems/arthais/image/manager/api/controllers/ImageController.java
+++ src/main/java/systems/arthais/image/manager/api/controllers/ImageController.java
@@ -1,6 +1,8 @@
 package systems.arthais.image.manager.api.controllers;
 
 import java.io.InputStream;
+import java.util.Arrays;
+import java.util.List;
 import java.util.UUID;
 
 import org.springframework.core.io.InputStreamResource;
@@ -16,38 +18,74 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+
 @RestController
 @RequestMapping("/images")
 public class ImageController {
-
+	
+	private static final List<String> ALLOWED_CONTENT_TYPES = Arrays.asList("image/jpeg", "image/png", "image/svg+xml");
+	private static final long MAX_FILE_SIZE = 10 * 1024 * 1024; // 10MB
+	
 	// Injeção de dependências para o serviço de imagem (a ser implementado)
 	// @Autowired
 	// private ImageService imageService;
+	
+    private boolean isContentTypeAllowed(MultipartFile file) {
+        return ALLOWED_CONTENT_TYPES.contains(file.getContentType());
+    }
 
-	@PostMapping
-	public ResponseEntity<String> uploadImage(@RequestParam("image") MultipartFile imageFile) {
-		UUID imageId = UUID.randomUUID(); // Gerar UUID único para a imagem
-		// Lógica para processar o upload da imagem
+    private boolean isFileSizeValid(MultipartFile file) {
+        return file.getSize() <= MAX_FILE_SIZE;
+    }
 
-		return ResponseEntity.ok(imageId.toString());
-	}
+    @PostMapping
+    public ResponseEntity<String> uploadImage(
+    		@NotEmpty @RequestParam("image") MultipartFile imageFile) {
+        if (!isContentTypeAllowed(imageFile)) {
+            return ResponseEntity.badRequest().body("Formato de arquivo não suportado. Formatos válidos: " + ALLOWED_CONTENT_TYPES);
+        }
+        if (!isFileSizeValid(imageFile)) {
+            return ResponseEntity.badRequest().body("Tamanho do arquivo excede o limite de " + (MAX_FILE_SIZE / 1024 / 1024) + "MB.");
+        }
 
-	@PutMapping("/{id}")
-	public ResponseEntity<String> updateImage(@PathVariable UUID id, @RequestParam("image") MultipartFile imageFile) {
-		// Lógica para atualizar a imagem com base no UUID
+        UUID imageId = UUID.randomUUID(); // Gerar UUID único para a imagem
+        // Lógica para processar o upload da imagem
 
-		return ResponseEntity.ok("Imagem atualizada com sucesso!");
-	}
+        return ResponseEntity.ok(imageId.toString());
+    }
 
-	@DeleteMapping("/{id}")
-	public ResponseEntity<String> deleteImage(@PathVariable UUID id) {
-		// Lógica para excluir a imagem com base no UUID
+    @PutMapping("/{id}")
+    public ResponseEntity<String> updateImage(
+    		@PathVariable UUID id, 
+    		@NotEmpty @RequestParam("image") MultipartFile imageFile) {
+    	
+        if (!isContentTypeAllowed(imageFile)) {
+            return ResponseEntity.badRequest().body("Formato de arquivo não suportado. Formatos válidos: " + ALLOWED_CONTENT_TYPES);
+        }
+        if (!isFileSizeValid(imageFile)) {
+            return ResponseEntity.badRequest().body("Tamanho do arquivo excede o limite de " + (MAX_FILE_SIZE / 1024 / 1024) + "MB.");
+        }
 
-		return ResponseEntity.ok("Imagem excluída com sucesso!");
-	}
+        // Lógica para atualizar a imagem com base no UUID
 
+        return ResponseEntity.ok("Imagem atualizada com sucesso!");
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<String> deleteImage(
+    		@NotNull @PathVariable UUID id) {
+         // Lógica para excluir a imagem com base no UUID
+
+        return ResponseEntity.ok("Imagem excluída com sucesso!");
+    }
+    
 	@GetMapping("/{id}")
-	public ResponseEntity<InputStreamResource> getImage(@PathVariable UUID id) {
+	public ResponseEntity<InputStreamResource> getImage(
+			@PathVariable UUID id) {
+         
 		// Lógica para recuperar a imagem com base no UUID
 		InputStream imageStream = null; // Obter o InputStream da imagem
 
---
+++ src/main/java/systems/arthais/image/manager/api/exceptions/ImageNotFoundException.java
@@ -0,0 +1,14 @@
+package systems.arthais.image.manager.api.exceptions;
+
+public class ImageNotFoundException extends RuntimeException {
+
+	private static final long serialVersionUID = 1L;
+
+	public ImageNotFoundException(String message) {
+		super(message);
+	}
+
+	public ImageNotFoundException() {
+		super("A imagem solicitada não foi encontrada.");
+	}
+}
